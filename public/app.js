072cec1d207b61be791e541903302542aaed0aba
Cristiam Diaz <c.diaz@udla.edu.co>
2018-03-15 14:40:00 -0500

    Feat: add socket.io

--- public/app.js
+++ public/app.js
@@ -1,6 +1,7 @@
 const vApp = {
 
   init: () => {
+    let socket = io.connect('http://localhost:3000/chat', { 'forceNew': true });
     vApp.render("<h1>Yet Another Chat!</h1>", document.querySelector('#brand'));
   },
 
--- src/server.js
+++ src/server.js
@@ -1,9 +1,16 @@
 let express = require('express');
 let app = express();
+let server = require('http').Server(app);
+let io = require('socket.io')(server);
+let chat = io.of('/chat')
 let port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/../public'));
 
-app.listen(port, function () {
-  console.log('Server listening on port '+ port);
+chat.on('connection', (socket) => {
+  console.log(`Connection established from: ${socket.id}`)
+});
+
+server.listen(port, () => {
+  console.log(`Server listening on port: ${port}`);
 });
